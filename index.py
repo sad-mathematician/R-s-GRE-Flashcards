f59c798e09b4820823faf4901d6a94cfe91e7634
Sad Mathematician <134358999+sad-mathematician@users.noreply.github.com>
2024-07-20 09:35:00 +0530

    Update index.py

--- index.py
+++ index.py
@@ -16,26 +16,26 @@ meaning = data.iloc[st.session_state.randw]['definition']
 st.write(word)
 
 # buttons
-count = 0
+# count = 0
 seemeaning_msg = 'See what this means'
 
 def NextWord():
     st.session_state.randw = random.randint(1, 324)
     st.experimental_rerun()  # Rerun the script to update the displayed word
 
-if count == 1:
-    seemeaning_msg = 'Next word'
+# if count == 1:
+#     seemeaning_msg = 'Next word'
     
 seemeaning = st.button(seemeaning_msg)
 
 if seemeaning:
     st.write(meaning)
-    count += 1
-    print(count)
+    # count += 1
+    # print(count)
 
 
-if count == 2:
-    NextWord()
-
-# if st.button('Next word'):
+# if count == 2:
 #     NextWord()
+
+if st.button('Next word'):
+    NextWord()
